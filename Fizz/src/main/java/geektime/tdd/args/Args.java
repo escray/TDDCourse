9c3a4eb749e18a1b9ff1b0b93d5109cc1ae40c59
escray <zescray@gmail.com>
2022-03-24 15:30:58 +0800

    remove build directory

--- Fizz/src/main/java/geektime/tdd/args/Args.java
+++ Fizz/src/main/java/geektime/tdd/args/Args.java
@@ -27,8 +27,7 @@ public class Args {
 
     private static Map<Class<?>, OptionParser> PARSERS = Map.of(
             boolean.class, new BooleanParser(),
-            int.class, IntegerParser.createIntegerParser(),
-            String.class, new IntegerParser(String::valueOf)
+            int.class, new SingleValuedOptionParser<>(Integer::parseInt),
+            String.class, new SingleValuedOptionParser<>(String::valueOf)
     );
-
 }
