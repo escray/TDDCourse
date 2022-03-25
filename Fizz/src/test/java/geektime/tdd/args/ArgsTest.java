65031d9d1180ff8c4780d59fab3ed1b1626cf00b
escray <zescray@gmail.com>
2022-03-25 13:54:10 +0800

    ch03 finished

--- Fizz/src/test/java/geektime/tdd/args/ArgsTest.java
+++ Fizz/src/test/java/geektime/tdd/args/ArgsTest.java
@@ -12,32 +12,25 @@ public class ArgsTest {
     // single option:
     // - Bool -l
 
-    // - Integer -p 8080
-    @Test
-    public void should_parse_int_as_option_value(){
-        IntOption option = Args.parse(IntOption.class, "-p", "8080");
-        assertEquals(8080, option.port());
-    }
-
-
-
-    // - String -d /usr/logs
-    @Test
-    public void should_parse_string_as_option_value() {
-        StringOption option = Args.parse(StringOption.class, "-d", "/usr/logs");
-        assertEquals("/usr/logs", option.directory());
-    }
-
-    static record IntOption(@Option("p") int port) {}
-
-    static record StringOption(@Option("d") String directory) {}
-    // multi options: -l -p 8080 -d /usr/logs
     @Test
     public void should_parse_multi_options() {
         MultiOptions options = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
         assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("/usr/logs", options.directory());
     }
 
+    static record MultiOptions(@Option("l")boolean logging, @Option("p")int port, @Option("d")String directory) {
+    }
+
+    @Test
+    public void should_throw_illegal_option_exception_if_annotation_not_present() {
+        IllegalOptionException e = assertThrows(IllegalOptionException.class, () -> Args.parse(OptionsWithoutAnnotation.class, "-l", "-p", "8080", "-d", "/usr/logs"));
+        assertEquals("port", e.getParameter());
+    }
+
+    static record OptionsWithoutAnnotation(@Option("l") boolean logging, int port, @Option("d") String directory){
+    }
     // BooleanOptionParserTest:
     // sad path:
     // TODO: - bool -l t / -l t f
@@ -85,8 +78,7 @@ public class ArgsTest {
         assertArrayEquals(new int[]{1, 2, -3, 5}, options.decimals());
     }
 
-    static record MultiOptions(@Option("l")boolean logging, @Option("p")int port, @Option("d")String directory) {
-    }
+
 
     static record ListOptions(@Option("g") String[] group, @Option("d") int[] decimals) {
     }
