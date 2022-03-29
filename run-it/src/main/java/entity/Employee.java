a7bc92286d000c0f04530963ed0e57e02d2d997a
escray <zescray@gmail.com>
2022-03-29 21:59:45 +0800

    create a simple Jakarta Persistence Application

---
+++ run-it/src/main/java/entity/Employee.java
@@ -0,0 +1,44 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name = "FNAME")
+    private String firstName;
+    @Column(name = "LNAME")
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fName) {
+        this.firstName = fName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lName) {
+        this.lastName = lName;
+    }
+
+    public Employee() {
+    }
+}
