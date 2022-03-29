fb0beb3dfd28d3f552181ebbc1dd2a329e5ecd19
escray <zescray@gmail.com>
2022-03-29 14:07:22 +0800

    from TestApplication to StudentRepositoryTest with the wired error, end of video 00-02

--- StudentDemo/src/main/java/geektime/tdd/model/TestApplication.java
+++ StudentDemo/src/main/java/geektime/tdd/model/TestApplication.java
@@ -7,24 +7,23 @@ import java.util.Optional;
 
 public class TestApplication {
     public static void main(String[] args) {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("student");
-        EntityManager entityManager = factory.createEntityManager();
-
-        entityManager.getTransaction().begin();
-
-        StudentRepository studentRepository = new StudentRepository(entityManager);
-        Student john = studentRepository.save(new Student("john", "smith", "john.smith@email.com"));
-
-        entityManager.getTransaction().commit();
-
-        System.out.println(john.getId());
-
-        Optional<Student> loaded = studentRepository.findById(john.getId());
-
-        System.out.println(loaded);
-
-        System.out.println(studentRepository.findByEmail("john.smith@email.com"));
-        System.out.println(studentRepository.findByEmail("john.smith@email1.com"));
-
+//        EntityManagerFactory factory = Persistence.createEntityManagerFactory("student");
+//        EntityManager entityManager = factory.createEntityManager();
+//
+//        entityManager.getTransaction().begin();
+//
+//        StudentRepository studentRepository = new StudentRepository(entityManager);
+//        Student john = studentRepository.save(new Student("john", "smith", "john.smith@email.com"));
+//
+//        entityManager.getTransaction().commit();
+//
+//        System.out.println(john.getId());
+//
+//        Optional<Student> loaded = studentRepository.findById(john.getId());
+//
+//        System.out.println(loaded);
+//
+//        System.out.println(studentRepository.findByEmail("john.smith@email.com"));
+//        System.out.println(studentRepository.findByEmail("john.smith@email1.com"));
     }
 }
