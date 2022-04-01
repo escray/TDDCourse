e33a80bc59b199b2e4699da6df24e9a1b6f583c3
escray <zescray@gmail.com>
2022-04-01 21:56:33 +0800

    ApiTest failed, end of video 05-04

--- run-it/src/test/java/geektime/tdd/model/StudentsTest.java
+++ run-it/src/test/java/geektime/tdd/model/StudentsTest.java
@@ -3,14 +3,17 @@ package geektime.tdd.model;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StudentsTest {
     private EntityManagerFactory factory;
@@ -35,6 +38,15 @@ public class StudentsTest {
             manager.persist(student);
             return student;
         }
+
+        public Optional<Student> findById(long id) {
+            return Optional.ofNullable(manager.find(Student.class, id));
+        }
+
+        public Optional<Student> findByEmail(String email) {
+            TypedQuery<Student> query = manager.createQuery("SELECT s FROM Student s WHERE s.email = :email", Student.class);
+            return query.setParameter("email", email).getResultList().stream().findFirst();
+        }
     }
 
     @AfterEach
@@ -47,6 +59,8 @@ public class StudentsTest {
     // save
     @Test
     public void should_save_student_to_db() {
+        int before = manager.createNativeQuery("select id, first_name from STUDENT s").getResultList().size();
+
         // exercise
         manager.getTransaction().begin();
         Student saved = students.save(new Student("john", "smith", "john.smith@email.com"));
@@ -55,18 +69,47 @@ public class StudentsTest {
         // verify
         List result = manager.createNativeQuery("select id, first_name, last_name, email from STUDENT s").getResultList();
 
-        assertEquals(1, result.size());
+        // weak point
+        assertEquals(before + 1, result.size());
 
         Object[] john = (Object[]) result.get(0);
 
-//        assertEquals(saved.getId(), john[0]);
+        assertEquals(saved.getId(), john[0]);
         assertEquals(saved.getFirstName(), john[1]);
         assertEquals(saved.getLastName(), john[2]);
         assertEquals(saved.getEmail(), john[3]);
     }
+
     // findById
+    @Test
+    public void should_be_able_to_load_saved_student_by_id() {
+        manager.getTransaction().begin();
+        Student john = students.save(new Student("john", "smith", "john.smith@email.com"));
+        manager.getTransaction().commit();
+
+        Optional<Student> loaded = students.findById(john.getId());
+
+        assertTrue(loaded.isPresent());
+
+        assertEquals(john.getFirstName(), loaded.get().getFirstName());
+        assertEquals(john.getLastName(), loaded.get().getLastName());
+        assertEquals(john.getEmail(), loaded.get().getEmail());
+        assertEquals(john.getId(), loaded.get().getId());
+    }
 
     // findByEmail
+    public void should_be_able_to_load_saved_student_by_email() {
+        manager.getTransaction().begin();
+        Student john = students.save(new Student("john", "smith", "john.smith@email.com"));
+        manager.getTransaction().commit();
 
+        Optional<Student> loaded = students.findByEmail(john.getEmail());
 
+        assertTrue(loaded.isPresent());
+
+        assertEquals(john.getFirstName(), loaded.get().getFirstName());
+        assertEquals(john.getLastName(), loaded.get().getLastName());
+        assertEquals(john.getEmail(), loaded.get().getEmail());
+        assertEquals(john.getId(), loaded.get().getId());
+    }
 }
