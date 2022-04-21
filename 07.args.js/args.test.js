c647e8f4fc4e8674ded1a6eaafe4c5015f1b559b
escray <zescray@gmail.com>
2022-04-22 07:11:42 +0800

    code review 02, end of video 11B-01

--- 07.args.js/args.test.js
+++ 07.args.js/args.test.js
@@ -43,6 +43,61 @@ describe('Args parser', () => {
     it('should fetch undefined if no flag match', () => {
       expect(opt(['-p', 'a'])).toBeUndefined();
     });
+
+    it('should call type to handle values', () => {
+      let opt = option('l', (values) => 1);
+      expect(opt(['-l', 'a', 'b'])).toEqual(1);
+    });
+  });
+
+  describe('bool', () => {
+    let type = bool();
+    it('should return true, if empty array given', () => {
+      expect(type([])).toBeTruthy();
+    });
+
+    it('shoudl return false, if undefined given', () => {
+      expect(type(undefined)).toBeFalsy();
+    });
+  });
+
+  describe('int', () => {
+    let type = int(-17);
+    it('should return 1, if array with single given', () => {
+      expect(type(['1'])).toEqual(1);
+    });
+
+    it('should return default value, if undefined given', () => {
+      expect(type(undefined)).toEqual(-17);
+    });
+
+    it ('should shrow exception, if more than 1 value present', () => {
+      expect(() => type(['1', '2'])).toThrowError('too many values');
+    });
+
+    it('should throw exception, if no value present', () => {
+      expect(() => type([])).toThrowError('too few values');
+    });
+  });
+
+  describe('string', () => {
+    let type = string('default');
+
+    it('should return string value, if array with single give', () => {
+      expect(type(['value'])).toEqual('value');
+    });
+
+    it('should return default value, if undefined given', () => {
+      expect(type(undefined)).toEqual('default');
+    });
+
+    it ('should throw exception, if more then 1 value present', () => {
+      expect(() => type(["1", "2"])).toThrowError("too many values");
+    });
+
+    it('should throw exception, if no value present', () => {
+      expect(() => type([])).toThrowError('too few values');
+    });
   });
 });
 
@@ -57,17 +112,42 @@ function option(flag, type) {
     if (nextIndex == -1) {
       nextIndex = args.length;
     }
-    return args.slice(index + 1, nextIndex);
+    return type(args.slice(index + 1, nextIndex));
   }
 }
 
-function bool() {
+function bool(defaultValue = false) {
+  return function (args) {
+    if (!args) {
+      return defaultValue;
+    }
+    if (args.length > 0) {
+      throw 'too many values';
+    }
+    return true;
+  }
 }
 
-function int() {
+function int(defaultValue = 0) {
+  return function (args) {
+    if (!args) {
+      return defaultValue;
+    }
+    if (args.length > 1) throw 'too many values';
+    if (args.length < 1) throw 'too few values';
+    return parseInt(args[0]);
+  }
 }
 
-function string() {
+function string(defaultValue = '') {
+  return function (args) {
+    if (!args) {
+      return defaultValue;
+    }
+    if (args.length === 1) {
+      return args[0];
+    }
+  }
 }
 
 function parse(schema, args) {
