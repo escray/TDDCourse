65c9d59b2a45c0974c68a19038252f3e0f443e1e
escray <zescray@gmail.com>
2022-04-25 20:59:54 +0800

    pass test through evil method

---
+++ 01.di.container/src/main/java/geektime/tdd/di/Context.java
@@ -0,0 +1,30 @@
+package geektime.tdd.di;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+    private Map<Class<?>, Object> components= new HashMap<>();
+    private final Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
+
+    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
+        components.put(type, instance);
+    }
+
+    public <ComponentType> ComponentType get(Class<ComponentType> type) {
+        if (components.containsKey(type)) {
+            return (ComponentType) components.get(type);
+        }
+        Class<?> implementation = componentImplementations.get(type);
+        try {
+            return (ComponentType)implementation.getConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <ComponentType, ComponentImplementation extends ComponentType>
+    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
+        componentImplementations.put(type, implementation);
+    }
+}
