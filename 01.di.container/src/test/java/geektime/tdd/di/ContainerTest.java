22a09b7281dab11b876f347acc484fae95a957e3
escray <zescray@gmail.com>
2022-04-26 20:23:11 +0800

    end of video 13-01

--- 01.di.container/src/test/java/geektime/tdd/di/ContainerTest.java
+++ 01.di.container/src/test/java/geektime/tdd/di/ContainerTest.java
@@ -1,5 +1,6 @@
 package geektime.tdd.di;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +11,14 @@ public class ContainerTest {
 
     static class ComponentWithDefaultConstructor implements Component {
         public ComponentWithDefaultConstructor() {
-
         }
+    }
+
+    Context context;
+
+    @BeforeEach
+    public void setup() {
+        context = new Context();
     }
 
     @Nested
@@ -19,33 +26,23 @@ public class ContainerTest {
         // TODO: instance
         @Test
         public void should_bin_type_to_a_specific_instance() {
-            Context context = new Context();
-
-            Component instance = new Component() {
-
-            };
+            Component instance = new Component() {};
             context.bind(Component.class, instance);
-
             assertSame(instance, context.get(Component.class));
         }
 
         // TODO: actract class
-
         // TODO: interface
-
         @Nested
         public class ConstructorInjection {
             // TODO: No args constructor
             @Test
             public void should_bind_type_to_a_class_with_default_constructor() {
-                Context context = new Context();
                 context.bind(Component.class, ComponentWithDefaultConstructor.class);
                 Component instance = context.get(Component.class);
                 assertNotNull(instance);
                 assertTrue(instance instanceof ComponentWithDefaultConstructor);
             }
-
-
 
             // TODO: with dependencies
 
