ba1d4170ebac26ebdb1117bb52aa7305bb114fcd
escray <zescray@gmail.com>
2022-05-12 20:42:57 +0800

    end of ch18

--- 01.di.container/src/main/java/geektime/tdd/di/ConstructorInjectionProvider.java
+++ 01.di.container/src/main/java/geektime/tdd/di/ConstructorInjectionProvider.java
@@ -1,36 +1,52 @@
 package geektime.tdd.di;
 
+import jakarta.inject.Inject;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
 
 class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
-    private Class<?> componentType;
     private Constructor<T> injectConstructor;
-    private boolean constructive = false;
 
-    public ConstructorInjectionProvider(Class<?> componentType, Constructor<T> injectConstructor) {
-        this.componentType = componentType;
-        this.injectConstructor = injectConstructor;
+    public ConstructorInjectionProvider(Class<T> component) {
+        this.injectConstructor = getInjectConstructor(component);
+    }
+
+    private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
+        List<Constructor<?>> injectConstructors = stream(implementation.getConstructors())
+                .filter(c -> c.isAnnotationPresent(Inject.class)).toList();
+
+        if (injectConstructors.size() > 1) throw new IllegalComponentException();
+
+        return (Constructor<Type>) injectConstructors.stream()
+                .findFirst().orElseGet(() -> {
+                    try {
+                        return implementation.getConstructor();
+                    } catch (NoSuchMethodException e) {
+                        throw new IllegalComponentException();
+                    }
+                });
     }
 
     @Override
     public T get(Context context) {
-        if (constructive) throw new CyclicDependenciesFoundException(componentType);
         try {
-            constructive = true;
             Object[] dependencies = stream(injectConstructor.getParameters())
-                    .map(p -> context.get(p.getType())
-                            .orElseThrow(() -> new DependencyNotFoundException(componentType, p.getType())))
+                    .map(p -> context.get(p.getType()).get())
                     .toArray(Object[]::new);
             return injectConstructor.newInstance(dependencies);
-        } catch (CyclicDependenciesFoundException e) {
-            throw new CyclicDependenciesFoundException(componentType, e);
         } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
-        } finally {
-            constructive = false;
         }
+    }
+
+    @Override
+    public List<Class<?>> getDependencies() {
+        return stream(injectConstructor.getParameters()).map(Parameter::getType).collect(Collectors.toList());
     }
 }
