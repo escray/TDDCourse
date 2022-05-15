d28cb03794cbac45c1afe221296a8c3f44cacf53
escray <zescray@gmail.com>
2022-05-15 20:55:21 +0800

    extract two traverse method

--- 01.di.container/src/main/java/geektime/tdd/di/InjectionProvider.java
+++ 01.di.container/src/main/java/geektime/tdd/di/InjectionProvider.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
@@ -38,17 +39,12 @@ class InjectionProvider<T> implements ComponentProvider<T> {
     @Override
     public T get(Context context) {
         try {
-            Constructor<T> constructor = this.injectConstructor;
-            Object[] dependencies = toDependencies(context, constructor);
-            T instance = this.injectConstructor.newInstance(dependencies);
+            T instance = injectConstructor.newInstance(toDependencies(context, injectConstructor));
             for (Field field : injectFields) {
-                field.set(instance, context.get(field.getType()).get());
+                field.set(instance, toDependency(context, field));
             }
             for (Method method : injectMethods) {
-                method.invoke(instance,
-                        stream(method.getParameterTypes())
-                        .map(t -> context.get(t).get())
-                                .toArray(Object[]::new));
+                method.invoke(instance, toDependencies(context, method));
             }
             return instance;
         } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
@@ -56,44 +52,55 @@ class InjectionProvider<T> implements ComponentProvider<T> {
         }
     }
 
-    private Object[] toDependencies(Context context, Constructor<T> constructor) {
-        return stream(constructor.getParameterTypes())
-                .map(t -> context.get(t).get())
-                .toArray(Object[]::new);
-    }
-
     @Override
     public List<Class<?>> getDependencies() {
-        return concat(concat(stream(injectConstructor.getParameters())
-                                .map(Parameter::getType),
+        return concat(concat(stream(injectConstructor.getParameters()).map(Parameter::getType),
                         injectFields.stream().map(Field::getType)),
-                injectMethods.stream()
-                        .flatMap(m -> stream(m.getParameterTypes())))
+                injectMethods.stream().flatMap(m -> stream(m.getParameterTypes())))
                 .toList();
     }
 
     private static <T> List<Field> getInjectFields(Class<T> component) {
+        BiFunction<List<Field>, Class<?>, List<Field>> function = InjectionProvider::getC;
+        return traverse(component, function);
+    }
+
+    private static <T> List<Field> traverse(Class<T> component, BiFunction<List<Field>, Class<?>, List<Field>> function) {
         List<Field> injectFields = new ArrayList<>();
         Class<?> current = component;
         while (current != Object.class) {
-            injectFields.addAll(injectable(current.getDeclaredFields()).toList());
+            injectFields.addAll(function.apply(injectFields, current));
             current = current.getSuperclass();
         }
         return injectFields;
     }
 
     private static <T> List<Method> getInjectMethods(Class<T> component) {
+        BiFunction<List<Method>, Class<?>, List<Method>> function = (methods, current) -> getC(component, methods, current);
+        List<Method> injectMethods = traverse1(component, function);
+        Collections.reverse(injectMethods);
+        return injectMethods;
+    }
+
+    private static <T> List<Method> traverse1(Class<T> component, BiFunction<List<Method>, Class<?>, List<Method>> function) {
         List<Method> injectMethods = new ArrayList<>();
         Class<?> current = component;
         while (current != Object.class) {
-            injectMethods.addAll(injectable(current.getDeclaredMethods())
-                    .filter(m -> isOverrideByInjectMethod(injectMethods, m))
-                    .filter(m -> isOverrideByNoInjectMethod(component, m))
-                    .toList());
+            injectMethods.addAll(function.apply(injectMethods, current));
             current = current.getSuperclass();
         }
-        Collections.reverse(injectMethods);
         return injectMethods;
+    }
+
+    private static <T> List<Method> getC(Class<T> component, List<Method> injectMethods, Class<?> current) {
+        return injectable(current.getDeclaredMethods())
+                .filter(m -> isOverrideByInjectMethod(injectMethods, m))
+                .filter(m -> isOverrideByNoInjectMethod(component, m))
+                .toList();
+    }
+
+    private static List<Field> getC(List<Field> fields, Class<?> current) {
+        return injectable(current.getDeclaredFields()).toList();
     }
 
     private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
@@ -104,14 +111,16 @@ class InjectionProvider<T> implements ComponentProvider<T> {
             throw new IllegalComponentException();
         }
 
-        return (Constructor<Type>) injectConstructors.stream()
-                .findFirst().orElseGet(() -> {
-                    try {
-                        return implementation.getDeclaredConstructor();
-                    } catch (NoSuchMethodException e) {
-                        throw new IllegalComponentException();
-                    }
-                });
+        return (Constructor<Type>) injectConstructors.stream().findFirst()
+                .orElseGet(() -> defaultConstructor(implementation));
+    }
+
+    private static <Type> Constructor<Type> defaultConstructor(Class<Type> implementation) {
+        try {
+            return implementation.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new IllegalComponentException();
+        }
     }
 
     private static <T extends AnnotatedElement> Stream<T> injectable(T[] declaredFields) {
@@ -132,5 +141,14 @@ class InjectionProvider<T> implements ComponentProvider<T> {
 
     private static boolean isOverrideByInjectMethod(List<Method> injectMethods, Method m) {
         return injectMethods.stream().noneMatch(o -> isOverride(o, m));
+    }
+
+    private static Object toDependency(Context context, Field field) {
+        return context.get(field.getType()).get();
+    }
+
+    private static Object[] toDependencies(Context context, Executable executable) {
+        return stream(executable.getParameterTypes())
+                .map(t -> context.get(t).get()).toArray(Object[]::new);
     }
 }
