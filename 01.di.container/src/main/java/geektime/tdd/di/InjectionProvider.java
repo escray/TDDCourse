7bf4f60370621f0030bf444aa8c419388c98be79
escray <zescray@gmail.com>
2022-05-15 21:07:39 +0800

    end of ch23

--- 01.di.container/src/main/java/geektime/tdd/di/InjectionProvider.java
+++ 01.di.container/src/main/java/geektime/tdd/di/InjectionProvider.java
@@ -54,7 +54,7 @@ class InjectionProvider<T> implements ComponentProvider<T> {
 
     @Override
     public List<Class<?>> getDependencies() {
-        return concat(concat(stream(injectConstructor.getParameters()).map(Parameter::getType),
+        return concat(concat(stream(injectConstructor.getParameterTypes()),
                         injectFields.stream().map(Field::getType)),
                 injectMethods.stream().flatMap(m -> stream(m.getParameterTypes())))
                 .toList();
