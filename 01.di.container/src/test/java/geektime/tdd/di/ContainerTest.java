4dc970b8fa6ce8338b31dd006d9ab8a0ed44f5c7
escray <zescray@gmail.com>
2022-05-16 13:07:42 +0800

    begin of ch24

--- 01.di.container/src/test/java/geektime/tdd/di/ContainerTest.java
+++ 01.di.container/src/test/java/geektime/tdd/di/ContainerTest.java
@@ -1,10 +1,33 @@
 package geektime.tdd.di;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 
 public class ContainerTest {
+    ContextConfig config;
+
+    @BeforeEach
+    public void setup() {
+        config = new ContextConfig();
+    }
+
     @Nested
     public class DependenciesSelection {
+        @Nested
+        public class ProviderType {
+            // Context
+            // TODO: could get Provider<T> from context
+
+            // InjectionProvider
+            // TODO: support inject constructor
+            // TODO: support inject field
+            // TODO: support inject method
+        }
+
+        @Nested
+        public class Qualifier {
+
+        }
     }
 
     @Nested
