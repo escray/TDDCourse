95823f129b531bd255725a9dc0dc4d9c0f251c52
escray <zescray@gmail.com>
2022-05-30 12:38:17 +0800

    DependencyCheck and CyclicDependency part finished

--- 01.di.container/src/main/java/geektime/tdd/di/CyclicDependenciesFoundException.java
+++ 01.di.container/src/main/java/geektime/tdd/di/CyclicDependenciesFoundException.java
@@ -5,13 +5,14 @@ import java.util.List;
 import java.util.Set;
 
 public class CyclicDependenciesFoundException extends RuntimeException {
-    private Set<Class<?>> components = new HashSet<>();
+    private Set<Component> components = new HashSet<>();
 
-    public CyclicDependenciesFoundException(List<Class<?>> visiting) {
+    public CyclicDependenciesFoundException(List<Component> visiting) {
         components.addAll(visiting);
     }
 
     public Class<?>[] getComponents() {
-        return components.stream().toArray(Class<?>[]::new);
+//        return components.stream().toArray(Class<?>[]::new);
+        return components.stream().map(Component::type).toArray(Class<?>[]::new);
     }
 }
